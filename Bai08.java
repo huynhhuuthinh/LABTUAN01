4bf4c63fe0d3314e9cbea4f709930adbfb5f0d4e
huynhhuuthinh <huynhquochuy82265@gmail.com>
2025-04-01 16:32:28 +0700

    Add files via upload

---
+++ Bai08.java
@@ -0,0 +1,24 @@
+package LABTUAN1;
+import java.util.Scanner;
+public class Bai08 {
+    public static void main(String[] args) {
+        Scanner HT= new Scanner(System.in);
+        int so, socuoi;
+        while (true) {
+            System.out.print("Nhập số bất kì:");
+         so = HT.nextInt();
+         socuoi = so%10;
+            if (so > 0) {
+                break;
+            }
+            else {
+                System.out.println("Nhập số dương");
+            }
+        }
+        while (so >= 10) {
+             so /= 10;
+        }
+        System.out.println("Số đầu là: " + so);
+        System.out.println("Số cuối là: " + socuoi);
+}
+}
